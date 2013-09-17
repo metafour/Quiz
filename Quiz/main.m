8b1cc028c8696c6d5db12c1f94314f2fbe865eec
Camron Schwoegler <camron@metafour.org>
2013-09-17 14:04:10 -0600

    Initial Commit

---
+++ Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Camron Schwoegler on 9/17/13.
+//  Copyright (c) 2013 Camron Schwoegler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "QuizAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([QuizAppDelegate class]));
+    }
+}
